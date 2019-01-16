dfcf06947db6b2241bebaa167c35dc77eb510245
selfcamp-JM <jakhongir.mamadov@gmail.com>
2019-01-16 08:20:28 +0300

    Updates

---
+++ precache-manifest.e78f56d3b487087328bcfd724be45460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "8db10d00b8c95261df97",
+    "url": "/todolist/static/js/main.8db10d00.chunk.js"
+  },
+  {
+    "revision": "8fefe52a4ba43897ab5a",
+    "url": "/todolist/static/js/1.8fefe52a.chunk.js"
+  },
+  {
+    "revision": "8db10d00b8c95261df97",
+    "url": "/todolist/static/css/main.1310616e.chunk.css"
+  },
+  {
+    "revision": "8fefe52a4ba43897ab5a",
+    "url": "/todolist/static/css/1.72cb1600.chunk.css"
+  },
+  {
+    "revision": "aee5b639b384580f377a091482527020",
+    "url": "/todolist/index.html"
+  }
+];
